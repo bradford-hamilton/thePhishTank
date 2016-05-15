a0ab942de547a82e48413192a254bedeee062acf
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-05-15 13:36:13 -0600

    change html to jade files and organize files

--- app.js/server.js
+++ app.js/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
+var jade = require('jade');
 
 var passport = require('passport');
 var passportLocal = require('passport-local');
