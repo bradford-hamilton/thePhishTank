54a8cc881de463714042097923582e190973684b
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-05-14 20:14:16 -0600

    add ajax for news on profile page

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+  /* Phish news ajax call */
+  $.ajax({
+    url: 'https://api.phish.net/api.js?api=2.0&method=pnet.news.get&format=json',
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'jsonp',
+    success: function(data) {
+      return data;
+    },
+    error: function(err) {
+      alert(err);
+    }
+  /* Use returned data */
+  }).done(function(data) {
+    var dates = [],
+        authors = [],
+        titles = [],
+        bodys = [];
+    /* Loop through data to store in arrays */
+    var len = data.length;
+    for (var i = 0; i < len; i++) {
+      dates.push( data[i].pubdate );
+      authors.push( data[i].postedby );
+      titles.push( data[i].title );
+      bodys.push( data[i].txt );
+      /* Append to page */
+      $('.news ul').append('<li></li>');
+      $('li').eq(i).append('<hr/><br><br>');
+      $('li').eq(i).append( "<h2 class='text-center'>" + titles[i] + "</h2 class>" );
+      $('li').eq(i).append('<br><br>');
+      $('li').eq(i).append( bodys[i] );
+    }
+
+  });
+
+
+
+});
