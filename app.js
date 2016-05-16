03189549af91aacc8f525cec3e0b38590fc2e658
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-05-16 13:39:30 -0600

    add working register page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
+    User = require('./models/User'),
     app = express(),
     bodyParser = require('body-parser'),
     session = require('express-session'),
@@ -8,78 +8,91 @@ var express = require('express'),
     LocalStrategy = require('passport-local').Strategy;
 
 
-mongoose.connect('mongodb://localhost/test', function(err) {
-  if (err) {
-    return console.log(err);
-  }
 
-  //Prepare mongoose schema
-  var userSchema = new Schema({
-    username: String,
-    email: String,
-    password: String
-  });
-  var User = mongoose.model('User', userSchema);
+mongoose.connect('mongodb://localhost/test');
 
-  // Set views
-  app.set('view engine', 'jade');
-  app.set('views', './');
+// Static assets
+app.use(express.static('./public'));
 
-  // Set middleware
-  app.use( bodyParser.urlencoded({ extended: false }) );
-  app.use(session({
-    secret: 'super-secret',
-    resave: true,
-    saveUninitialized: true
-  }));
+// Set views
+app.set('view engine', 'jade');
+app.set('views', './views');
 
-  // Set passport middleware
-  app.use( passport.initialize() );
-  app.use( passport.session() );
+// Set middleware
+app.use( bodyParser.urlencoded({ extended: false }) );
+app.use(session({
+  secret: 'super-secret',
+  resave: true,
+  saveUninitialized: true
+}));
 
-  // Serialize user
-  passport.serializeUser(function(user, done) {
-    return done(null, user._id);
-  });
+// Set passport middleware
+app.use( passport.initialize() );
+app.use( passport.session() );
 
-  // Deserialize user
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
-    });
-  });
-
-  // Register logic
-  passport.use('registerUser', new LocalStrategy(
-    { passReqToCallback: true },
-    function(req, username, password, done) {
-      var newUser = new User({
-        username: username,
-        password: password,
-        email: req.body.email
-      });
-      newUser.save(function(err) {
-        if (err) {
-          return done(err);
-        }
-        return done(null, newUser);
-      });
-    }
-  ));
-
-  // Define route
-  app.route('/register')
-    .get(function(request, response) {
-      response.render('register');
-    })
-    .post(function(request, response, next) {
-      passport.authenticate('registerUser', function(err, user, info){
-        if (err) {
-          return response.send({ err: err, info: info });
-        }
-        response.send(user);
-      })(request, response, next);
-    });
-
-  app.listen(1337);
+// Serialize user
+passport.serializeUser(function(user, done) {
+  return done(null, user._id);
 });
+
+// Deserialize user
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// Register logic
+passport.use('registerUser', new LocalStrategy(
+  { passReqToCallback: true },
+  function(req, username, password, done) {
+    var newUser = new User({
+      username: username,
+      password: password,
+      email: req.body.email
+    });
+    newUser.save(function(err) {
+      if (err) {
+        return done(err);
+      }
+      return done(null, newUser);
+    });
+  }
+));
+
+// Define register route
+app.route('/register')
+  .get(function(request, response) {
+    response.render('register');
+  })
+  .post(function(request, response, next) {
+    passport.authenticate('registerUser', function(err, user, info){
+      if (err) {
+        return response.send({ err: err, info: info });
+      }
+      response.send(user);
+    })(request, response, next);
+  });
+
+// Define login route
+app.route('/').get(function(request, response) {
+  response.render('index');
+});
+
+app.route('/news').get(function(request, response) {
+  if (!request.user) {
+    response.redirect('/');
+    return;
+  }
+  response.render('index');
+});
+
+app.route('/profile').get(function(request, response) {
+  // if (!request.user) {
+  //   response.redirect('/');
+  //   return;
+  // }
+  response.render('profile');
+});
+
+app.listen(1337);
